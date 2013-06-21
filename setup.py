d779c9e94d03da5d328fa6cd23183e6709220bf2
Ryan Wilson-Perkin <ryanwilsonperkin@gmail.com>
2013-06-21 16:01:08 -0400

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,29 +1,24 @@
 import os
-import re
-import sys
-import subprocess
-
 from setuptools import setup
-from distutils.sysconfig import get_python_lib
 
-import vero_python
+import vero
 
 # Don't generate .pyc, .pyo files on import
 os.environ['PYTHONDONTWRITEBYTECODE'] = '1'
 
 packages = [
-    'vero_python'
+    'vero'
 ]
 requires = ['requests==1.2.3']
 
 setup(
-    name='vero_python',
+    name='vero',
     description='Python wrapper for Vero API',
     long_description=open('README.md').read(),
-    version=vero_python.__version__,
+    version=vero.__version__,
     author=open('AUTHORS.md').read(),
     author_email='rwilsonperkin@waveaccounting.com',
-    url='https://github.com/waveaccounting/vero-python-client',
+    url='https://github.com/waveaccounting/vero-python',
     packages=packages,
     package_data={'': ['LICENSE.md']},
     include_package_data=True,
