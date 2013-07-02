fd25fbe8f9bcec501c47347179bd7cecfc3d57f1
Nick Presta <nick@nickpresta.ca>
2013-07-02 11:53:54 -0400

    Allow setup.py test to work as expected.

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ packages = [
     'vero'
 ]
 requires = ['requests==1.2.3']
+tests_require = ['mock==1.0.1']
 
 setup(
     name='vero',
@@ -21,5 +22,7 @@ setup(
     include_package_data=True,
     zip_safe=True,
     install_requires=requires,
+    tests_require=tests_require,
+    test_suite='vero.tests.client_test',
     license=open('LICENSE.rst').read(),
 )
