5b931075d53db61c904f93c15c00ad53408d4b69
Ryan Wilson-Perkin <ryanwilsonperkin@gmail.com>
2015-06-05 10:10:51 -0400

    Bump patch version of vero-python

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,6 +1,14 @@
 Vero Python
 ===========
 
+Version 1.1.4
+-------------
+
+Released on June 5, 2015
+
+- Bumped requests library to 2.7.0
+- Adds support for Python 3.4
+
 Version 1.1.3
 -------------
 
--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     name='vero',
     description='Python wrapper for Vero API',
     long_description=open('README.rst').read(),
-    version='1.1.3',
+    version='1.1.4',
     author=open('AUTHORS.rst').read(),
     author_email='opensource@waveaccounting.com',
     url='https://github.com/waveaccounting/vero-python',
